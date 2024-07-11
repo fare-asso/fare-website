0f107d51f3282855b74e9eea3e3ee004f5cbcd51
Saunier Martin <martin.saunier@etudiant.univ-rennes1.fr>
2024-07-11 18:49:48 +0200

    Ajout de la page de mentions légales

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,10 +5,8 @@ export default function Footer() {
     return(
         <footer className="bg-black text-white w-full h-32 mt-auto p-8 flex flex-col justify-end">
             <div className="w-full flex flex-row justify-between items-end">
-                <div className="opacity-75 text-xs flex flex-row space-x-4 hover:[&>a]:underline">
-                    <Link href="/">Politique de confidentialité</Link>
-                    <Link href="/">Conditions générales</Link>
-                    <Link href="/">Politique des cookies</Link>
+                <div className="opacity-75 text-xs flex flex-row space-x-1 md:space-x-4 hover:[&>a]:underline">
+                    <Link href="/mentions-legales">Mentions Légales</Link>
                     <span>© 2024 FAHB. Tous droits réservés.</span>
                 </div>
                 <SocialLinks />
