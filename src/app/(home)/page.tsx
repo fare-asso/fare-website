3fec4598b4d54c04ccd34132dc5a463ccf881ebe
Saunier Martin <martin.saunier35@gmail.com>
2024-08-22 15:23:16 +0200

    Added content to the home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,32 +3,94 @@ import { Metadata } from "next";
 import DiscordWidget from "@/components/public/discordWidget";
 
 import WelcomeImage from "../../../public/welcome.jpg";
+import Link from "next/link";
+import LinkButton from "@/components/public/link";
+import AssociationMapCaller from "@/components/public/associations/map/associationMapCaller";
+import prisma from "@/helpers/db";
 
 export const metadata: Metadata = {
   title: "Accueil | FAHB"
 }
 
-export default function Home() {
+export default async function Home() {
+
+  const associations = await prisma.association.findMany()
+
   return (
-    <div>
+    <div className="w-full md:w-[90%] flex flex-col items-center">
       
-      <div className="flex flex-col md:flex-row mt-8 mb-4 w-full">
-        <Image src={WelcomeImage} alt="Image des membres du bureau" className="w-full rounded-lg" />
-        <div className="w-full flex flex-col mt-2 md:mt-0 md:mx-2 [&_span]:text-black">
-          <div className="rounded-lg bg-yellow-400 flex flex-col items-center justify-center p-2 h-full mb-2">
-            <span className="text-3xl font-semibold">17</span>
-            <span className="text-xl">Associations étudiantes</span>
+      {/* Welcome picture and key numbers */}
+      <div className="w-full flex flex-col md:flex-row mt-8 mb-4">
+        <Image src={WelcomeImage} alt="Image des membres du bureau" className="w-full rounded-xl" />
+        <div className="w-full flex flex-col mt-2 md:mt-0 md:ml-4 [&_span]:text-white flex-shrink-[2]">
+          <div className="rounded-xl bg-fahbyellow flex flex-col items-center justify-center p-4 md:p-2 h-full mb-2 md:mb-4">
+            <span className="text-2xl md:text-[2.5rem] font-semibold">17</span>
+            <span className="text-xl md:p-1 opacity-95">Associations étudiantes</span>
           </div>
-          <div className="rounded-lg bg-yellow-400 flex flex-col items-center justify-center p-2 h-full mb-2">
-            <span className="text-3xl font-semibold">88 000</span>
-            <span className="text-xl">Étudiant.e.s</span>
+          <div className="rounded-xl bg-fahbyellow flex flex-col items-center justify-center p-4 md:p-2 h-full mb-2 md:mb-4">
+            <span className="text-2xl md:text-[2.5rem] font-semibold">88 000</span>
+            <span className="text-xl md:p-1 opacity-95">Étudiant.e.s</span>
           </div>
-          <div className="rounded-lg bg-yellow-400 flex flex-col items-center justify-center p-2 h-full">
-            <span className="text-3xl font-semibold">28</span>
-            <span className="text-xl">Élu.e.s universitaires & CROUS</span>
+          <div className="rounded-xl bg-fahbyellow flex flex-col items-center justify-center p-4 md:p-2 h-full">
+            <span className="text-2xl md:text-[2.5rem] font-semibold">28</span>
+            <span className="text-xl md:p-1 opacity-95">Élu.e.s universitaires & CROUS</span>
           </div>
         </div>
       </div>
+
+      {/* Who are we */}
+      <div className="my-10 w-full flex flex-col items-center">
+        <div className="w-full md:w-[80%] rounded-xl flex flex-col items-start justify-between px-12 py-8 bg-black text-lg text-white">
+          <h2 className="text-2xl mb-2 font-semibold">Qui sommes-nous ?</h2>
+          <p className="">
+            La Fédération des Associations de Haute-Bretagne (FAHB) est une organisation humaniste et militante qui représente les étudiant.e.s d'Ille-et-Vilaine et des Côtes d'Armor.
+            Indépendante de tout parti politique, elle œuvre chaque jour pour améliorer la vie des 88 000 étudiant.e.s du territoire grâce à des projets construits "par et pour les étudiant.e.s".
+          </p>
+          <div className="w-full flex items-center flex-col pt-8">
+            <LinkButton href="/about" title="En savoir +" className="bg-white text-black"/>
+          </div>
+        </div>
+      </div>
+      
+
+      {/* Last articles */}
+      <div className="my-10 w-full flex flex-col">
+        <h2 className="text-2xl font-semibold mb-2">
+          Actualités
+        </h2>
+        <div className="w-full flex flex-row space-x-2">
+          <div className="rounded-xl h-32 w-24 bg-gray-600">
+
+          </div>
+          <div className="rounded-xl h-32 w-24 bg-gray-600">
+
+          </div>
+        </div>
+
+      </div>
+
+      {/* Le réseau */}
+      <div className="my-10 w-full flex flex-col">
+        <h2 className="text-2xl font-semibold mb-2">
+          Notre réseau
+        </h2>
+        <div className="flex flex-col items-center">
+          <AssociationMapCaller associations={associations} />
+        </div>
+        
+      </div>
+
+      {/* Les évènements à venir */}
+      <div className="my-10 w-full flex flex-col">
+        <h2 className="text-2xl font-semibold mb-2">
+          Les évènements à venir
+        </h2>
+        <div className="w-full flex flex-row space-x-2">
+          
+        </div>
+        
+      </div>
+
 
 
       <DiscordWidget />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config = {
     },
     extend: {
       colors: {
+        fahbyellow: "rgb(255, 203, 0)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
