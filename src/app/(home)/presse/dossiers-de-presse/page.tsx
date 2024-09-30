c3e16d4d6ab285981690e4b2bf4c093422bdb08b
Saunier Martin <blasster35@gmail.com>
2024-09-30 16:44:37 +0200

    Fix largeur page

--- src/app/(home)/presse/dossiers-de-presse/page.tsx
+++ src/app/(home)/presse/dossiers-de-presse/page.tsx
@@ -17,7 +17,7 @@ export default async function DossiersDePresse() {
         <div className="flex flex-col items-center justify-start w-full px-4 md:px-8 lg:px-16 mb-20">
             <h1 className="py-12 sm:py-24 text-4xl font-bold text-center">Dossiers de presse</h1>
 
-            <div className='w-full flex flex-col items-center space-y-2'>
+            <div className='w-3/4 flex flex-col items-center space-y-2'>
                 {
                     dossiers.length > 0 ?
                     dossiers.map((dossier) => <DossierDePresseCard key={dossier.id} dossier={dossier} />)
