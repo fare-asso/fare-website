508ed426b12903d7bc19d895162eb7e0b51291d3
Saunier Martin <blasster35@gmail.com>
2025-01-23 16:48:37 +0100

    feat(ticket): add delete functionality for bagad'Asso tickets with user role verification

---
+++ src/actions/bagadAsso/deleteTicketAction.tsx
@@ -0,0 +1,35 @@
+"use server";
+
+import prisma from "@/helpers/db";
+import getCurrentUserRole from "@/helpers/user/role";
+import { revalidatePath } from "next/cache";
+
+export default async function deleteBagadAssoTicketAction(
+    ticketId: number,
+): Promise<{
+    success?: boolean;
+    error?: string;
+}> {
+    /* SUPER IMPORTANT : Auth and role verifications */
+    const { role, error } = await getCurrentUserRole();
+    if (error) return { error: "Echec de l'authentification de l'utilisateur" };
+    if (role != "ADMIN")
+        return {
+            error: "Vous devez avoir les droits administrateur pour effectuer cette opération.",
+        };
+
+    try {
+        await prisma.bagadAssoTicket.delete({
+            where: {
+                id: ticketId,
+            },
+        });
+
+        revalidatePath("/dashboard/bagadAsso");
+
+        return { success: true };
+    } catch (error: any) {
+        console.error(error.message);
+        return { error: "Echec de la suppression du ticket" };
+    }
+}
--- src/components/dashboard/bagadAsso/ticketCard.tsx
+++ src/components/dashboard/bagadAsso/ticketCard.tsx
@@ -1,28 +1,85 @@
+"use client";
+
+import deleteBagadAssoTicketAction from "@/actions/bagadAsso/deleteTicketAction";
+import { useToast } from "@/components/ui/use-toast";
 import { BagadAssoTicket } from "@prisma/client";
-import { format } from "date-fns";
+import { format, isBefore } from "date-fns";
 import Link from "next/link";
-import { FaDownload } from "react-icons/fa6";
+import { MdDelete } from "react-icons/md";
+import LoadingRing from "../loadingRing";
+import { useState } from "react";
 
 export default function BagadAssoTicketCard({
     ticket,
 }: {
     ticket: BagadAssoTicket;
 }) {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const { toast } = useToast();
+
+    const onDelete = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        setIsLoading(true);
+
+        const response = await deleteBagadAssoTicketAction(ticket.id);
+
+        if (response.error) {
+            // Toast Error
+            toast({
+                variant: "destructive",
+                description: `${response.error}`,
+                title: "Erreur",
+            });
+        } else {
+            // Toast Success
+            toast({
+                variant: "default",
+                description: "Le ticket a été supprimé avec succès.",
+                title: "Succès",
+            });
+        }
+        setIsLoading(false);
+    };
+
+    const isExpired = isBefore(new Date(ticket.eventDate), new Date());
+
     return (
-        <div className="w-full border border-foreground rounded-lg p-4 flex flex-row items-center justify-between">
-            <span
-                className="bg-black text-sm px-1 rounded-md text-white"
-                style={{ fontFamily: "Consolas, serif" }}
+        <div
+            className={`w-full border border-gray-300 rounded-lg p-4 flex flex-col md:flex-row items-center justify-between shadow-sm hover:shadow-md transition-shadow duration-300 ${isExpired ? "bg-red-100" : ""}`}
+        >
+            <div className="flex flex-col md:flex-row items-center w-full md:w-auto">
+                <span className="hidden sm:block bg-gray-800 text-sm px-2 py-1 rounded-md text-white font-mono mb-2 md:mb-0 md:mr-4">
+                    #{ticket.id}
+                </span>
+                <Link
+                    href={`/dashboard/bagadAsso/tickets/${ticket.id}`}
+                    className="underline text-black hover:text-gray-600 transition-colors duration-300 mb-2 md:mb-0 md:mr-4"
+                >
+                    {ticket.assocation.length > 12
+                        ? `${ticket.assocation.substring(0, 12)}...`
+                        : ticket.assocation}
+                </Link>
+                <span className="text-gray-600 mb-2 md:mb-0 md:mr-4">
+                    {format(ticket.creationDate, "dd/MM/yyyy")}
+                </span>
+                <span
+                    className={`text-sm font-semibold ${isExpired ? "text-red-600" : "text-green-600"}`}
+                >
+                    {isExpired ? "Dépassé" : "A venir"}
+                </span>
+            </div>
+            <button
+                title={`Supprimer le ticket n°${ticket}`}
+                onClick={onDelete}
+                className="flex flex-row  items-center justify-center bg-red-500 text-white px-2 py-2 rounded-md hover:bg-red-600 transition-colors duration-300 mt-2 md:mt-0"
             >
-                #{ticket.id}
-            </span>
-            <Link
-                href={`/dashboard/bagadAsso/tickets/${ticket.id}`}
-                className="underline hover:font-bold transition-all"
-            >
-                {ticket.assocation}
-            </Link>
-            <span>{format(ticket.creationDate, "dd/MM/yyyy")}</span>
+                {isLoading ? (
+                    <LoadingRing className="!m-0" />
+                ) : (
+                    <MdDelete size="20" />
+                )}
+            </button>
         </div>
     );
 }
