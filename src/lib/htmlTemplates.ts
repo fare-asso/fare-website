55ddfcea7213564fbb4bd5c9cd5c1d46ed431550
Saunier Martin <blasster35@gmail.com>
2025-01-23 16:03:11 +0100

    feat(emailTemplates): enhance bagad'Asso ticket email template with event details

--- src/lib/htmlTemplates.ts
+++ src/lib/htmlTemplates.ts
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 export const adhesionEmailTemplate = (associationName: string): string => `
             <p>Une nouvelle adhésion a été reçue pour l'association <strong>${associationName}</strong>.</p>
             <p>Vous pouvez consulter les détails de cette adhésion dans le <a href="${process.env.NEXT_PUBLIC_SITE_URL}/dashboard/adhesions">tableau de bord des adhésions</a>.</p>
@@ -6,7 +8,9 @@ export const adhesionEmailTemplate = (associationName: string): string => `
 export const bagadAssoTicketEmailTemplate = (
     ticketId: number,
     associationName: string,
+    eventDate: Date,
+    eventName: string,
 ): string => `
-            <p>Un nouveau tilcet bagad'Asso à été soumit par l'association <strong>${associationName}</strong>.</p>
-            <p>Vous pouvez consulter les détails du ticket sur le <a href="${process.env.NEXT_PUBLIC_SITE_URL}/dashboard/adhesions">tableau de bord des tickets bagadAssos</a>.</p>
+            <p>Un nouveau ticket Bagad'Asso à été soumit par l'association <strong>${associationName}</strong> pour l'évènement <strong>${eventName}</strong> qui aura lieu le <strong>${format(eventDate, "dd/MM/yyy")}</strong>.</p>
+            <p>Vous pouvez consulter les détails du ticket sur le <a href="${process.env.NEXT_PUBLIC_SITE_URL}/dashboard/bagadAsso/tickets/${ticketId}">tableau de bord des tickets bagadAssos</a>.</p>
             `;
