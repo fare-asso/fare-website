ae08f871747c922360b91332eb2f6cc55c0897d6
Saunier Martin <blasster35@gmail.com>
2025-01-28 04:11:44 +0100

    feat(contact): enhance ContactForm with loading state and error handling

--- src/components/public/contact/contactForm.tsx
+++ src/components/public/contact/contactForm.tsx
@@ -1,6 +1,46 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Input from "./input";
+import clsx from "clsx";
+import TextArea from "./textarea";
+import { Contact, ContactSchema } from "@/schemas/contact";
+import { useState } from "react";
+import LoadingRing from "@/components/dashboard/loadingRing";
+import submitContactFormAction from "@/actions/contact/submitContactFormAction";
+
 export default function ContactForm() {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [success, setSuccess] = useState<boolean>(false);
+
+    const form = useForm<Contact>({
+        resolver: zodResolver(ContactSchema),
+    });
+
+    const getFirstError = () => {
+        const firstError = Object.entries(form.formState.errors)[0];
+        return firstError ? firstError[1].message : null;
+    };
+
+    const onSubmit = async (data: Contact) => {
+        setIsLoading(true);
+
+        const res = await submitContactFormAction(data);
+
+        res.errors?.forEach(({ message, field }) => {
+            form.setError(field as keyof Contact, { type: "manual", message });
+        });
+
+        if (res.success) {
+            setIsLoading(false);
+            setSuccess(true);
+            form.reset();
+        }
+    };
+
     return (
-        <div className="flex flex-col md:flex-row w-full md:w-[70%] bg-black mt-12 p-8 rounded-3xl">
+        <div className="flex flex-col md:flex-row w-full md:w-[70%] bg-black mt-12 p-4 md:p-8 rounded-3xl">
             {/* Text Section */}
             <div className="flex flex-col justify-center w-full md:w-1/2 pr-0 md:pr-8 mb-6 md:mb-0">
                 <h2 className="text-2xl text-white font-semibold mb-4">
@@ -13,15 +53,22 @@ export default function ContactForm() {
             </div>
 
             {/* Form Section */}
-            <form className="flex flex-col w-full md:w-1/2 space-y-4 text-white">
+            <form
+                className="flex flex-col w-full md:w-1/2 space-y-4 text-white"
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
                 {/* First name + Last name */}
                 <div className="flex flex-col sm:flex-row gap-4 w-full">
-                    <input
+                    <Input
+                        {...form.register("firstName")}
+                        error={form.formState.errors.firstName}
                         type="text"
                         placeholder="Prénom"
                         className="flex-1 text-center bg-[#202124] py-3 px-4 rounded-xl focus:outline-none focus:ring-2 focus:ring-white/20 w-full"
                     />
-                    <input
+                    <Input
+                        {...form.register("lastName")}
+                        error={form.formState.errors.lastName}
                         type="text"
                         placeholder="Nom"
                         className="flex-1 text-center bg-[#202124] py-3 px-4 rounded-xl focus:outline-none focus:ring-2 focus:ring-white/20 w-full"
@@ -29,24 +76,43 @@ export default function ContactForm() {
                 </div>
 
                 {/* Email address */}
-                <input
+                <Input
+                    {...form.register("email")}
+                    error={form.formState.errors.email}
                     type="email"
                     placeholder="Email"
                     className="w-full text-center bg-[#202124] py-3 px-4 rounded-xl focus:outline-none focus:ring-2 focus:ring-white/20"
                 />
 
                 {/* Message */}
-                <textarea
+                <TextArea
+                    {...form.register("message")}
+                    error={form.formState.errors.message}
                     placeholder="Entrez votre message ici"
                     className="w-full text-center bg-[#202124] py-3 px-4 rounded-xl h-32 resize-none focus:outline-none focus:ring-2 focus:ring-white/20"
-                ></textarea>
+                ></TextArea>
 
                 {/* Submit button */}
                 <button
                     type="submit"
-                    className="w-full rounded-full bg-white/20 py-3 text-lg text-gray-200 hover:bg-white/30 transition-colors duration-200"
+                    disabled={isLoading || success}
+                    className={clsx(
+                        "w-full flex flex-row items-center justify-center rounded-full bg-white/20 py-3 text-lg text-gray-200 hover:bg-white/30 transition-colors duration-200",
+                        getFirstError() &&
+                            "!bg-red-500 hover:bg-red-500 cursor-not-allowed",
+                        success &&
+                            "!bg-green-500 hover:bg-green-500 cursor-default",
+                        isLoading && "cursor-wait",
+                    )}
                 >
-                    Envoyer
+                    {getFirstError() ??
+                        (isLoading ? (
+                            <LoadingRing className="!size-[28px]" />
+                        ) : success ? (
+                            "Demande envoyée"
+                        ) : (
+                            "Envoyer"
+                        ))}
                 </button>
             </form>
         </div>
