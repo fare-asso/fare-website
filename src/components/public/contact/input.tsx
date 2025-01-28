5d30759fcdd21ddb412682c516226b0415122af4
Saunier Martin <blasster35@gmail.com>
2025-01-28 04:11:18 +0100

    feat(contact): add Input and TextArea components with error handling

---
+++ src/components/public/contact/input.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { InputHTMLAttributes } from "react";
+
+import { RiErrorWarningFill } from "react-icons/ri";
+
+import React from "react";
+import { FieldError } from "react-hook-form";
+
+const Input = React.forwardRef<
+    HTMLInputElement,
+    InputHTMLAttributes<HTMLInputElement> & {
+        className?: string;
+        error?: FieldError;
+    }
+>(({ className, error, ...props }, ref) => {
+    return (
+        <div className="relative flex flex-row items-center justify-end w-full">
+            <input className={cn(className)} ref={ref} {...props} />
+
+            {/* Error */}
+            {error && ( // Afficher uniquement si une erreur est présente
+                <div className="absolute mr-3 h-full flex flex-row items-center">
+                    <RiErrorWarningFill
+                        className="text-red-500 group-hover:scale-105 transition-all"
+                        size={20}
+                        title={error.message} // Optionnel : pour afficher un tooltip avec l'erreur
+                    />
+                </div>
+            )}
+        </div>
+    );
+});
+
+export default Input;
---
+++ src/components/public/contact/textarea.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { InputHTMLAttributes } from "react";
+
+import { RiErrorWarningFill } from "react-icons/ri";
+
+import React from "react";
+import { FieldError } from "react-hook-form";
+
+const TextArea = React.forwardRef<
+    HTMLTextAreaElement,
+    InputHTMLAttributes<HTMLTextAreaElement> & {
+        className?: string;
+        error?: FieldError;
+    }
+>(({ className, error, ...props }, ref) => {
+    return (
+        <div className="relative flex flex-row items-center justify-end w-full">
+            <textarea className={cn(className)} ref={ref} {...props} />
+
+            {/* Error */}
+            {error && ( // Afficher uniquement si une erreur est présente
+                <div className="absolute flex flex-row items-end bottom-0 right-0 p-2">
+                    <RiErrorWarningFill
+                        className="text-red-500 group-hover:scale-105 transition-all"
+                        size={20}
+                        title={error.message} // Optionnel : pour afficher un tooltip avec l'erreur
+                    />
+                </div>
+            )}
+        </div>
+    );
+});
+
+export default TextArea;
