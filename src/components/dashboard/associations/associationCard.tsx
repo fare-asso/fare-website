70266a1855d2a5f7df2f360614ae3502769f41b7
Saunier Martin <blasster35@gmail.com>
2025-01-28 18:32:37 +0100

    refactor(associationCard): clean up unused imports and improve layout structure

--- src/components/dashboard/associations/associationCard.tsx
+++ src/components/dashboard/associations/associationCard.tsx
@@ -2,23 +2,10 @@
 
 import Image from "next/image";
 
-import { Button } from "@/components/ui/button";
-
-import { MouseEvent, useState } from "react";
-
-import { MdDelete } from "react-icons/md";
-
-import { useToast } from "@/components/ui/use-toast";
-
-// import EditMemberButton from "./editMemberButton";
-
-import clsx from "clsx";
-
 import { Association } from "@prisma/client";
 import EditAssociationButton from "./editAssociationButton";
 import SendInvitationLinkButton from "./sendInvitationLinkButton";
 import DeleteRepresentativeButton from "./deleteRepresentativeButton";
-import AssociationDropdownMenu from "./AssociationDropdownMenu";
 import DeleteAssociationButton from "./deleteAssociationButton";
 
 export default function AssociationCard({
@@ -28,36 +15,8 @@ export default function AssociationCard({
     association: Association;
     logoUrl: string;
 }) {
-    // const { toast } = useToast()
-
-    const [hidden, setIsHidden] = useState<boolean>(false);
-
-    // const handleDelete = async (event: MouseEvent<HTMLButtonElement>) => {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-
-    //     setIsHidden(true)
-    //     const res = await deleteMemberAction({id: member.id});
-    //     if(res.error) {
-    //         toast({
-    //             title: "Erreur",
-    //             variant: "destructive",
-    //             description: res.error
-    //         })
-    //     } else {
-    //         toast({
-    //             description: `Le membre ${member.firstName} ${member.lastName} a bien été supprimé`
-    //         })
-    //     }
-    // }
-
     return (
-        <div
-            className={clsx(
-                "flex h-min flex-col items-start rounded-lg border bg-card p-3 text-card-foreground shadow-sm",
-                hidden && "hidden",
-            )}
-        >
+        <div className="flex h-min w-full flex-col items-start rounded-lg border bg-card p-3 text-card-foreground shadow-sm">
             <div className="relative">
                 {/* Hover buttons */}
                 <div className="absolute flex h-full w-full flex-row items-start justify-end space-x-1 p-2 opacity-100 lg:opacity-0 lg:hover:opacity-100">
@@ -76,10 +35,10 @@ export default function AssociationCard({
                 />
             </div>
 
-            <div className="mb-[0.125rem] flex w-full flex-row space-x-1 font-medium text-card-foreground">
+            <div className="flex w-full flex-row space-x-1 overflow-hidden text-ellipsis text-nowrap font-medium text-card-foreground">
                 {association.name}
             </div>
-            <div className="inline-flex items-center whitespace-nowrap rounded-full bg-card-foreground px-2.5 py-0.5 text-xs font-semibold text-primary-foreground">
+            <div className="text-sm text-foreground/70">
                 {association.major}
             </div>
         </div>
