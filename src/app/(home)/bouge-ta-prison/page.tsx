86e399c87a10c00f78e86c7689e0d2e7246bfaa7
Saunier Martin <blasster35@gmail.com>
2025-01-30 20:35:48 +0100

    fix(bouge-ta-prison): simplify heading markup and enhance layout consistency

--- src/app/(home)/bouge-ta-prison/page.tsx
+++ src/app/(home)/bouge-ta-prison/page.tsx
@@ -19,9 +19,7 @@ export default function BougeTaPrison() {
 
             <div className="w-full max-w-4xl space-y-12">
                 <section>
-                    <h2 className="mb-4 text-2xl font-semibold">
-                        Qu'est-ce que le projet Bouge Ta Prison ?
-                    </h2>
+                    <h2>Qu'est-ce que le projet Bouge Ta Prison ?</h2>
                     <p className="mb-4">
                         Le projet Bouge Ta Prison est une initiative qui vise à
                         sensibiliser notamment les étudiant·e·s sur le monde
@@ -31,30 +29,38 @@ export default function BougeTaPrison() {
                     </p>
                 </section>
 
-                <div className="flex flex-col space-x-0 space-y-3 md:flex-row md:space-x-2 md:space-y-0 [&>img]:w-full [&>img]:rounded-xl [&>img]:object-cover md:[&>img]:w-1/2">
-                    <Image
-                        src={assemblee0}
-                        alt="Photo d'une conférence de sensibilisation du milieu carcéral à l'Université de Rennes 2 qui s'est déroulée en février 2024"
-                    />
-                    <Image
-                        src={assemblee1}
-                        alt="Deuxième photo de la conférence de sensibilisation du milieu carcéral à l'Université de Rennes 2"
-                    />
-                </div>
-
                 <section>
-                    <h2 className="mb-4 text-2xl font-semibold">
-                        Sensibilisation au milieu carcéral
-                    </h2>
+                    <h2>Sensibilisation au milieu carcéral</h2>
                     <p className="mb-4">
-                        Le projet organise des conférences pour sensibiliser au
-                        milieu carcéral. Par exemple, en février 2024, à
-                        l'Université de Rennes 2, deux visiteurs de prison sont
-                        venus témoigner de leurs expériences et expliquer le
-                        monde carcéral avec tous ses enjeux et ses difficultés.
+                        Afin de sensibiliser au milieu carcéral, le pôle
+                        organise des conférences. C'était le cas en février
+                        2024, à l'Université de Rennes 2, deux visiteurs de
+                        prison sont venus témoigner de leurs expériences et
+                        expliquer le monde carcéral avec tous ses enjeux et ses
+                        difficultés qu'il présente.
                     </p>
+
+                    <div className="mb-4 flex flex-col space-x-0 space-y-3 md:flex-row md:space-x-2 md:space-y-0 [&>img]:w-full [&>img]:rounded-xl [&>img]:object-cover md:[&>img]:w-1/2">
+                        <Image
+                            src={assemblee0}
+                            alt="Photo d'une conférence de sensibilisation du milieu carcéral à l'Université de Rennes 2 qui s'est déroulée en février 2024"
+                        />
+                        <Image
+                            src={assemblee1}
+                            alt="Deuxième photo de la conférence de sensibilisation du milieu carcéral à l'Université de Rennes 2"
+                        />
+                    </div>
+
                     <p className="mb-4">
-                        La FAHB fait également partie du collectif prison Rennes
+                        La FAHB fait également partie du{" "}
+                        <Link
+                            href="https://collectifprisonrennes.fr/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="link"
+                        >
+                            collectif prison Rennes
+                        </Link>{" "}
                         depuis début d'année 2024 et participe pour la deuxième
                         fois à l'organisation des{" "}
                         <strong>Journées Nationales des Prisons (JNP)</strong>{" "}
@@ -68,7 +74,7 @@ export default function BougeTaPrison() {
                 </section>
 
                 <section>
-                    <h2 className="mb-4 text-2xl font-semibold">
+                    <h2>
                         Réinsertion des personnes placées sous main de justice
                     </h2>
                     <p className="mb-4">
@@ -95,12 +101,33 @@ export default function BougeTaPrison() {
                         Pénitentiaire pour Hommes de Vezin-le-Coquet en binôme
                         pour suivre 12 détenu·e·s.
                     </p>
+
+                    {/* Intéressé pour devenir tuteur? */}
+                    <div className="flex h-auto w-full flex-col items-center pt-4">
+                        <div className="flex w-full flex-col rounded-xl bg-black p-8 text-white md:w-3/4">
+                            <h2 className="mb-2 text-lg font-semibold">
+                                Intéressé pour devenir tuteur?
+                            </h2>
+                            <p>
+                                La FAHB accueille de nouveaux membres partageant
+                                nos objectifs pour la vie étudiante. En nous
+                                rejoignant, vous aurez accès à notre réseau, nos
+                                ressources et notre soutien. <br />
+                                Pour plus d'informations sur l'adhésion, cliquez
+                                ci-dessous.
+                            </p>
+                            <Link
+                                href="/bouge-ta-prison/tutorat"
+                                className="ml-auto mt-4 w-full rounded-full border-white bg-white px-4 py-2 text-center font-semibold text-black transition-all hover:scale-105 md:w-1/3"
+                            >
+                                En savoir plus
+                            </Link>
+                        </div>
+                    </div>
                 </section>
 
                 <section>
-                    <h2 className="mb-4 text-2xl font-semibold">
-                        Couverture médiatique
-                    </h2>
+                    <h2>Couverture médiatique</h2>
                     <p className="mb-4">
                         Ces différents projets ont pu donner lieu à des articles
                         de presse dans le Ouest France, France 3 et des journaux
@@ -112,7 +139,7 @@ export default function BougeTaPrison() {
                                 href="https://france3-regions.francetvinfo.fr/bretagne/ille-et-vilaine/rennes/temoignage-si-je-peux-aider-un-peu-pour-leur-reinsertion-tant-mieux-pourquoi-cette-etudiante-accompagne-les-detenus-qui-reprennent-leurs-etudes-2912339.html"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="text-blue-600 hover:underline"
+                                className="link"
                             >
                                 Article France 3 Régions
                             </Link>
@@ -122,7 +149,7 @@ export default function BougeTaPrison() {
                                 href="https://www.campusmatin.com/vie-campus/experience-etudiante/une-supbox-pour-repenser-l-acces-a-l-enseignement-superieur-en-prison.html"
                                 target="_blank"
                                 rel="noopener noreferrer"
-                                className="text-blue-600 hover:underline"
+                                className="link"
                             >
                                 Article Campus Matin
                             </Link>
