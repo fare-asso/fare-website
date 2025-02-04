3244263e6cd8c7a481e93c61a3831f08afbc2f93
Saunier Martin <blasster35@gmail.com>
2025-02-04 22:54:34 +0100

    refactor(sidebar): simplify sidebar link rendering with a dynamic array

--- src/components/dashboard/sideBar.tsx
+++ src/components/dashboard/sideBar.tsx
@@ -25,31 +25,54 @@ export default function SideBar({ children }: { children: JSX.Element[] }) {
         setOpen(false);
     };
 
+    const links = [
+        {
+            href: "/dashboard/events",
+            title: "Evènements",
+        },
+        {
+            href: "/dashboard/associations",
+            title: "Associations",
+        },
+        {
+            href: "/dashboard/articles",
+            title: "Articles",
+        },
+        {
+            href: "/dashboard/communiques-de-presse",
+            title: "Presse",
+        },
+        {
+            href: "/dashboard/membres",
+            title: "Membres",
+        },
+        {
+            href: "/dashboard/bagadAsso",
+            title: "Bagad'Asso",
+        },
+        {
+            href: "/dashboard/adhesions",
+            title: "Adhésions",
+        },
+        {
+            href: "/dashboard/bouge-ta-prison",
+            title: "Bouge Ta Prison",
+        },
+    ];
+
     return (
         <div className="flex h-auto w-full flex-row items-center justify-between border-b p-4 lg:min-h-screen lg:w-1/6 lg:flex-col lg:border-b-0 lg:border-r">
             <div className="select-none text-lg font-semibold">Dashboard</div>
 
             <div className="hidden h-full flex-row items-center justify-center lg:mt-10 lg:flex lg:flex-1 lg:flex-col lg:justify-between lg:space-y-2">
                 <div className="flex flex-row items-center space-x-2 space-y-0 lg:flex-col lg:space-x-0 lg:space-y-2">
-                    <SideBarLink href="/dashboard/events" title="Evènements" />
-                    <SideBarLink
-                        href="/dashboard/associations"
-                        title="Associations"
-                    />
-                    <SideBarLink href="/dashboard/articles" title="Articles" />
-                    <SideBarLink
-                        href="/dashboard/communiques-de-presse"
-                        title="Presse"
-                    />
-                    <SideBarLink href="/dashboard/membres" title="Membres" />
-                    <SideBarLink
-                        href="/dashboard/bagadAsso"
-                        title="Bagad'Asso"
-                    />
-                    <SideBarLink
-                        href="/dashboard/adhesions"
-                        title="Adhésions"
-                    />
+                    {links.map((link) => (
+                        <SideBarLink
+                            href={link.href}
+                            title={link.title}
+                            key={link.href}
+                        />
+                    ))}
                 </div>
 
                 <div className="ml-2 flex flex-col items-center justify-center space-y-0 lg:ml-0 lg:space-y-2">
@@ -66,41 +89,14 @@ export default function SideBar({ children }: { children: JSX.Element[] }) {
                 <SheetContent className="lg:hidden">
                     <div className="flex h-full flex-col items-start justify-between space-y-2 pt-5">
                         <div className="flex flex-col items-start space-y-2">
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/events"
-                                title="Evènements"
-                            />
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/associations"
-                                title="Associations"
-                            />
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/articles"
-                                title="Articles"
-                            />
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/communiques-de-presse"
-                                title="Presse"
-                            />
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/membres"
-                                title="Membres"
-                            />
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/bagadAsso"
-                                title="Bagad'Asso"
-                            />
-                            <SideBarLink
-                                onClick={handleSidebarCollapse}
-                                href="/dashboard/adhesions"
-                                title="Adhésions"
-                            />
+                            {links.map((link) => (
+                                <SideBarLink
+                                    key={link.href}
+                                    href={link.href}
+                                    title={link.title}
+                                    onClick={handleSidebarCollapse}
+                                />
+                            ))}
                         </div>
 
                         <div className="flex w-full flex-col items-center justify-center space-y-0">
