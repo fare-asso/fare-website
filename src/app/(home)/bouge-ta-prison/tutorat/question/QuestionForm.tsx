aac7c13dab34f3064f43c63a2d58879e179d4ab4
Saunier Martin <blasster35@gmail.com>
2025-02-16 23:33:45 +0100

    feat(question-form): add QuestionForm component for tutor inquiries

---
+++ src/app/(home)/bouge-ta-prison/tutorat/question/QuestionForm.tsx
@@ -0,0 +1,269 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Alert } from "@/components/ui/alert";
+
+import {
+    Form,
+    FormField,
+    FormDescription,
+    FormItem,
+    FormLabel,
+    FormMessage,
+    FormControl,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    BTPTutorQuestionSchema,
+    BTPTutorQuestion,
+} from "@/schemas/bougeTaPrison";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import Captcha from "@/components/captcha/recaptcha";
+import LoadingRing from "@/components/dashboard/loadingRing";
+import { Textarea } from "@/components/ui/textarea";
+import submitTutorQuestion from "@/actions/bouge-ta-prison/submitTutorQuestion";
+
+export default function QuestionForm() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [success, setSuccess] = useState<boolean | undefined>(undefined);
+
+    const form = useForm<BTPTutorQuestion>({
+        resolver: zodResolver(BTPTutorQuestionSchema),
+        defaultValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            major: "",
+            studyYear: "L3",
+            message: "",
+        },
+    });
+
+    const onSubmit = async (data: BTPTutorQuestion) => {
+        setIsLoading(true);
+        const res = await submitTutorQuestion(data);
+
+        if (!res.success) {
+            res.errors?.forEach((error) => {
+                form.setError(Object.keys(error)[0] as keyof BTPTutorQuestion, {
+                    message: Object.values(error)[0],
+                });
+            });
+        }
+        setSuccess(res.success);
+        form.reset();
+        setIsLoading(false);
+    };
+
+    return (
+        <Card>
+            <CardHeader>
+                <h2>Vous avez une question sur le tutorat Bouge Ta Prison ?</h2>
+                <p className="text-sm">
+                    Si vous avez des questions concernant le tutorat Bouge Ta
+                    Prison, n'hésitez pas à nous les poser en remplissant le
+                    formulaire ci-dessous. Nous vous répondrons dans les plus
+                    brefs délais.
+                </p>
+            </CardHeader>
+            <CardContent>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4"
+                    >
+                        <FormField
+                            name="firstName"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Prénom</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Jean" {...field} />
+                                    </FormControl>
+                                    <FormMessage>
+                                        {
+                                            form.formState.errors.firstName
+                                                ?.message
+                                        }
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="lastName"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nom</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Martin"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage>
+                                        {
+                                            form.formState.errors.lastName
+                                                ?.message
+                                        }
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="email"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="jean.martin@example.com"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage>
+                                        {form.formState.errors.email?.message}
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="major"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Filière</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="Droit, Psychologie, etc."
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Votre filière d'études actuelle
+                                    </FormDescription>
+                                    <FormMessage>
+                                        {form.formState.errors.major?.message}
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            name="studyYear"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Année d'étude</FormLabel>
+                                    <FormControl>
+                                        <Select>
+                                            <FormControl>
+                                                <SelectTrigger className="w-full md:w-1/2">
+                                                    <SelectValue
+                                                        placeholder="Veuillez selectionner une année d'étude"
+                                                        {...field}
+                                                    />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectItem value="L3">
+                                                    Licence 3
+                                                </SelectItem>
+                                                <SelectItem value="M1">
+                                                    M1
+                                                </SelectItem>
+                                                <SelectItem value="M2">
+                                                    M2
+                                                </SelectItem>
+                                                <SelectItem value="other">
+                                                    Autres
+                                                </SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </FormControl>
+                                    <FormDescription>
+                                        Votre année d'étude prévue pour
+                                        2025-2026
+                                    </FormDescription>
+                                    <FormMessage>
+                                        {
+                                            form.formState.errors.studyYear
+                                                ?.message
+                                        }
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+
+                        <FormField
+                            name="message"
+                            control={form.control}
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Message</FormLabel>
+                                    <FormControl>
+                                        <Textarea
+                                            placeholder="Vous pouvez écrire votre message ici."
+                                            className="h-32 max-h-52"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage>
+                                        {form.formState.errors.message?.message}
+                                    </FormMessage>
+                                </FormItem>
+                            )}
+                        />
+
+                        {/* <Captcha onChange={() => console.log("Captcha")} /> */}
+
+                        <Button
+                            type="submit"
+                            variant="default"
+                            disabled={isLoading}
+                        >
+                            {isLoading ?
+                                <LoadingRing />
+                            :   null}
+                            Envoyer
+                        </Button>
+                    </form>
+                </Form>
+
+                {success === true ?
+                    <Alert
+                        variant="default"
+                        className="mt-4 flex flex-row items-center border-green-500 bg-green-100 text-green-900"
+                    >
+                        <span>
+                            Votre question a bien été envoyée. Merci pour votre
+                            intérêt!
+                        </span>
+                    </Alert>
+                :   null}
+                {success === false ?
+                    <Alert
+                        variant="destructive"
+                        className="mt-4 flex flex-row items-center"
+                    >
+                        <span>
+                            Une erreur est survenue lors de l'envoi de votre
+                            question. Veuillez réessayer.
+                        </span>
+                    </Alert>
+                :   null}
+            </CardContent>
+        </Card>
+    );
+}
