63f1abe7ef656fede700a5188f5a55aff8822981
Saunier Martin <blasster35@gmail.com>
2025-02-16 23:34:01 +0100

    feat(tutorat): add inquiry section with link for tutor questions

--- src/app/(home)/bouge-ta-prison/tutorat/page.tsx
+++ src/app/(home)/bouge-ta-prison/tutorat/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import TutorApplicationForm from "./TutorApplicationForm";
 
 // Create metadata for the page
@@ -77,6 +78,28 @@ export default function Tutorat() {
                     <p className="mb-4">
                         Vous avez jusqu'au 30 mai pour postuler.
                     </p>
+
+                    {/* Intéressé pour devenir tuteur? */}
+                    <div className="flex h-auto w-full flex-col items-center py-4 !pb-8">
+                        <div className="flex w-full flex-col rounded-xl bg-black p-8 text-white md:w-3/4">
+                            <h2 className="mb-2 text-lg font-semibold">
+                                Vous avez des questions ?
+                            </h2>
+                            <p>
+                                Vous vous demandez si vous êtes éligible pour le
+                                tutorat ? Vous avez des questions sur le
+                                processus de candidature ? Vous souhaitez en
+                                savoir plus ?
+                            </p>
+                            <Link
+                                href="/bouge-ta-prison/tutorat/question"
+                                className="ml-auto mt-4 w-full rounded-full border-white bg-white px-4 py-2 text-center font-semibold text-black transition-all hover:scale-105 md:w-1/3"
+                            >
+                                J'ai une question
+                            </Link>
+                        </div>
+                    </div>
+
                     <TutorApplicationForm />
                 </section>
             </div>
