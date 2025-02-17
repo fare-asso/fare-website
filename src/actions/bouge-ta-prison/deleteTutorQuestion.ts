f4e7b32dcad0674992d154ff59e46e9cadd391a2
Saunier Martin <blasster35@gmail.com>
2025-02-17 18:03:56 +0100

    feat(tutor-question): add deleteTutorQuestion action for removing tutor questions

---
+++ src/actions/bouge-ta-prison/deleteTutorQuestion.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import prisma from "@/helpers/db";
+import { revalidatePath } from "next/cache";
+
+export default async function deleteTutorQuestion(
+    id: number,
+): Promise<{ success?: boolean; error?: string }> {
+    // Delete the application
+    try {
+        const deletedApplication = await prisma.bTPTutorQuestion.delete({
+            where: {
+                id,
+            },
+        });
+    } catch (error) {
+        return { error: "Echec de la suppression de la question" };
+    }
+
+    revalidatePath("/dashboard/bouge-ta-prison?tab=questions");
+    return { success: true };
+}
