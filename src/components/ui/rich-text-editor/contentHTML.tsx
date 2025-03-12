786d2749a096bf8f3d40ace946c56d8515859637
Saunier Martin <blasster35@gmail.com>
2025-03-12 03:01:48 +0100

    feat(rich-text-editor): add ContentHTML component for rendering JSON content as HTML

---
+++ src/components/ui/rich-text-editor/contentHTML.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import jsonToHtml from "@/helpers/tiptap/jsonToHtml";
+import { JSONContent } from "@tiptap/react";
+
+export default function ContentHTML({ content }: { content: JSONContent }) {
+    console.log(content);
+    return <div dangerouslySetInnerHTML={{ __html: jsonToHtml(content) }} />;
+}
