a733d9f88505a3dff9cc1101dd57521f7e771976
Saunier Martin <blasster35@gmail.com>
2025-03-12 03:02:40 +0100

    refactor(article-list): remove unused imports and interface definition

--- src/components/dashboard/Articles/articleList.tsx
+++ src/components/dashboard/Articles/articleList.tsx
@@ -1,26 +1,8 @@
-import { createClient } from "@/helpers/supabase/server";
-
 import prisma from "@/helpers/db";
-
-import { Prisma } from "@prisma/client";
 
 import ArticleCard from "./articleCard";
 
-// import MemberCard from "./memberCard";
-
-export interface Article {
-    id: number;
-    title: string;
-    content: Prisma.JsonValue;
-    imagesPath: string[];
-    writtenOn: Date;
-    authorId: string;
-}
-
 export default async function ArticleList() {
-    // create supabase client
-    const supabase = createClient();
-
     // fetch all members from DB
     const articles = await prisma.article.findMany({
         orderBy: {
