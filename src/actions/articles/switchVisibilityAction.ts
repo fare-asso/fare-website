cce49fdae55434d4de4199da1b180a3ac87be984
Saunier Martin <blasster35@gmail.com>
2025-03-12 02:56:33 +0100

    feat(article): implement server action to toggle article visibility with admin checks

---
+++ src/actions/articles/switchVisibilityAction.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import prisma from "@/helpers/db";
+import getCurrentUserRole from "@/helpers/user/role";
+import { revalidatePath } from "next/cache";
+
+export default async function switchVisibilityAction(articleId: number) {
+    /* SUPER IMPORTANT : Auth and role verifications */
+    const { role, error } = await getCurrentUserRole();
+    if (error) return { error: "Echec de l'authentification de l'utilisateur" };
+    if (role != "ADMIN")
+        return {
+            error: "Vous devez avoir les droits administrateur pour effectuer cette opération.",
+        };
+
+    // Fetch current article
+    const article = await prisma.article.findUnique({
+        where: {
+            id: articleId,
+        },
+        select: {
+            published: true,
+        },
+    });
+
+    if (!article) {
+        throw new Error("Article not found");
+    }
+
+    await prisma.article.update({
+        where: {
+            id: articleId,
+        },
+        data: {
+            published: !article.published,
+        },
+    });
+
+    revalidatePath("/actualites");
+    revalidatePath("/dashboard/articles");
+}
