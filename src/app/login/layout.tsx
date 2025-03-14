1c093ca005031c9dfe18309e2c0dd2e7e7d0a0bd
Saunier Martin <blasster35@gmail.com>
2025-03-14 14:59:25 +0100

    fix(login-layout): remove duplicate html and body tags

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,8 +1,4 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Connection Admin",
@@ -15,12 +11,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body className={inter.className}>
-                <main className="flex min-h-screen flex-col items-center p-8">
-                    {children}
-                </main>
-            </body>
-        </html>
+        <main className="flex min-h-screen flex-col items-center p-8">
+            {children}
+        </main>
     );
 }
