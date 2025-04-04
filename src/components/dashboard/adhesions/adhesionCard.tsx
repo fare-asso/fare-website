4a8cd9194f1825add851dbe1655c6301b154dc59
Saunier Martin <blasster35@gmail.com>
2025-04-04 14:14:17 +0200

    fix(tailwindcss): remove duplicate outline properties on hover

--- src/components/dashboard/adhesions/adhesionCard.tsx
+++ src/components/dashboard/adhesions/adhesionCard.tsx
@@ -92,7 +92,7 @@ export default function AdhesionCard({ adhesion }: { adhesion: Adhesion }) {
 
     return (
         <div className={clsx("flex flex-col items-center", hidden && "hidden")}>
-            <div className="relative flex h-32 w-32 cursor-pointer items-center justify-center rounded-lg border bg-card p-6 text-card-foreground shadow-xs outline-offset-2 outline-black/30 hover:outline hover:outline-2">
+            <div className="bg-card text-card-foreground relative flex h-32 w-32 cursor-pointer items-center justify-center rounded-lg border p-6 shadow-xs outline-offset-2 outline-black/30 hover:outline-2">
                 {/* Hover buttons */}
                 <div className="absolute flex h-full w-full flex-row items-start justify-end space-x-1 p-1 opacity-100 md:opacity-0 md:hover:opacity-100">
                     <button
