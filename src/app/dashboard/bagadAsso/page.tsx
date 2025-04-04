a79937cba2a3e777cc926c96629195daf7890ebf
Saunier Martin <blasster35@gmail.com>
2025-04-04 03:24:59 +0200

    refactor(bagadAsso): extract the tabSwitch to a separate component

--- src/app/dashboard/bagadAsso/page.tsx
+++ src/app/dashboard/bagadAsso/page.tsx
@@ -9,36 +9,25 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import TabSwitcher from "./tabSwitcher";
 
 export default function BagadAsso() {
     return (
-        <Card className="flex h-full w-full flex-1 flex-col border-none">
-            <CardHeader>
+        <Card className="flex h-full w-full flex-1 flex-col border-none p-0 shadow-none">
+            <CardHeader className="p-0">
                 <CardTitle>Espace Bagad'Asso</CardTitle>
                 <CardDescription>
                     Espace de gestion des tickets et du matériel du projet
                     Bagad'Asso
                 </CardDescription>
             </CardHeader>
-            <CardContent className="flex-1 overflow-y-auto">
-                <Tabs
-                    defaultValue="tickets"
-                    className="flex h-full w-full flex-col items-center"
-                >
-                    <TabsList className="grid w-full grid-cols-2 md:w-1/2">
-                        <TabsTrigger value="tickets">Tickets</TabsTrigger>
-                        <TabsTrigger value="materials">Matériels</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="tickets" className="w-full flex-1">
-                        <Tickets />
-                    </TabsContent>
-                    <TabsContent value="materials" className="w-full flex-1">
-                        <Equipments />
-                    </TabsContent>
-                </Tabs>
+            <CardContent className="h-1/2 flex-1 p-0">
+                <TabSwitcher>
+                    <Tickets />
+                    <Equipments />
+                </TabSwitcher>
             </CardContent>
-            <CardFooter></CardFooter>
+            <CardFooter className="p-0"></CardFooter>
         </Card>
     );
 }
