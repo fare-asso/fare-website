c3112bbacd5f254cde487c1f81fa9e04d9853d07
Saunier Martin <blasster35@gmail.com>
2025-04-05 18:20:12 +0200

    feat(ui): enhance LoginPage layout with logo and improved structure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,15 +9,26 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 
-import { Button } from "@/components/ui/button";
+import { MdAdminPanelSettings } from "react-icons/md";
 
 import LoginForm from "@/components/dashboard/loginForm";
+import Image from "next/image";
+
+import FAHBLogo from "/public/logo.webp";
 
 export default function LoginPage() {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Connexion Admin</CardTitle>
+        <Card className="mx-auto mt-20 max-w-sm min-w-1/3">
+            <CardHeader className="flex flex-col items-start">
+                <Image
+                    src={FAHBLogo}
+                    alt="Logo de la FAHB"
+                    className="mx-auto mb-6 w-1/5 text-center"
+                />
+                <CardTitle className="flex items-center gap-1">
+                    <MdAdminPanelSettings size={25} />
+                    <span>Connexion Admin</span>
+                </CardTitle>
                 <CardDescription>
                     Accès réservé uniquement aux membres de la fédération
                 </CardDescription>
