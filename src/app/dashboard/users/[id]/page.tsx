d0a19dd419f49f0b77431b1534fecb4d5070cbd2
Saunier Martin <blasster35@gmail.com>
2025-04-14 23:31:17 +0200

    refactor(userPage): simplify layout and remove unnecessary elements from UserPage component

--- src/app/dashboard/users/[id]/page.tsx
+++ src/app/dashboard/users/[id]/page.tsx
@@ -2,8 +2,6 @@ import { Separator } from "@/components/ui/separator";
 import prisma from "@/helpers/db";
 import { UserPermissionsForm } from "./userPermissionForm";
 import { UserInfoForm } from "./userInfoForm";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 
 export default async function UserPage({
     params,
@@ -30,35 +28,16 @@ export default async function UserPage({
     const allPermissions = await prisma.permission.findMany();
 
     return (
-        <div className="flex h-full w-full flex-col px-4">
-            <Link
-                href="/dashboard/users"
-                className="mb-4 text-sm underline opacity-80 transition-all hover:font-bold"
-            >
-                &lsaquo; Retour aux utilisateurs
-            </Link>
-
-            {/* User Info */}
-            <div className="flex flex-col gap-2">
-                <h2 className="mb-4 text-2xl font-bold">
-                    Informations de l'utilisateur
-                </h2>
-                <UserInfoForm user={user} />
-            </div>
-
+        <div className="flex h-full w-full flex-col overflow-y-auto px-4 [&_h2]:font-bold">
+            <h2>Informations</h2>
+            <UserInfoForm user={user} />
             <Separator className="my-4" />
-
-            {/* Permissions */}
-            <div className="flex flex-col gap-2 pb-4">
-                <h2 className="!mb-2 text-2xl font-bold">Permissions</h2>
-                <UserPermissionsForm
-                    userId={user.id}
-                    userPermissions={user.permissions.map(
-                        (up) => up.permissionId,
-                    )}
-                    allPermissions={allPermissions}
-                />
-            </div>
+            <h2>Permissions</h2>
+            <UserPermissionsForm
+                userId={user.id}
+                userPermissions={user.permissions.map((p) => p.permission.id)}
+                allPermissions={allPermissions}
+            />
         </div>
     );
 }
