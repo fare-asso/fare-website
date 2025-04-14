6e497b85f71e2970dda132f4b6730b5c7cdd242d
Saunier Martin <blasster35@gmail.com>
2025-04-14 15:54:11 +0200

    feat(unauthorized-page): create UnauthorizedPage component for access denial message

---
+++ src/app/dashboard/unauthorized/page.tsx
@@ -0,0 +1,14 @@
+export default function UnauthorizedPage() {
+    return (
+        <div className="flex h-1/2 w-full flex-col items-center justify-center p-4 text-center">
+            <h1 className="text-2xl font-bold">⚠️ Accès refusé ⚠️</h1>
+            <p className="mt-4 text-lg">
+                Vous n'avez pas les permissions nécessaires pour accéder à cette
+                page.
+                <br />
+                Veuillez contacter un administrateur si vous pensez que c'est
+                une erreur.
+            </p>
+        </div>
+    );
+}
