38e00ecaefc744ced7a950e3cc62c5871ac7b12d
KimBlazter <blasster35@gmail.com>
2025-10-21 11:53:45 +0200

    fix(typing): remove explicit type annotation for cdpCards in CDPList

--- src/components/dashboard/CDP/CDPList.tsx
+++ src/components/dashboard/CDP/CDPList.tsx
@@ -18,7 +18,7 @@ export default async function CDPList() {
             </span>
         );
     } else {
-        const cdpCards: JSX.Element[] = communiques.map((cdp) => (
+        const cdpCards = communiques.map((cdp) => (
             <CdpCard
                 key={cdp.id}
                 cdp={cdp}
@@ -30,7 +30,7 @@ export default async function CDPList() {
         ));
 
         return (
-            <div className="h-full w-full overflow-y-auto rounded-lg border bg-card p-6 text-card-foreground shadow-xs">
+            <div className="bg-card text-card-foreground h-full w-full overflow-y-auto rounded-lg border p-6 shadow-xs">
                 <div className="grid h-auto w-full grid-cols-1 gap-4 overflow-y-auto p-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
                     {cdpCards.length > 0 ?
                         cdpCards
