b7ada050de074db75d15c35d4b0a3b6f19334ad8
KimBlazter <blasster35@gmail.com>
2025-10-24 00:06:28 +0200

    fix(home_page): update Discord invite link to new URL

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -118,7 +118,7 @@ export default async function Home() {
                         <p className="mb-2 text-center text-lg font-semibold">
                             Vidéo de présentation du&nbsp;
                             <a
-                                href="https://discord.gg/VNK9GcheFr"
+                                href="https://discord.gg/DtfxbHaqua"
                                 title="Lien vers Discord"
                                 className="text-blue-600 transition-all hover:text-blue-400"
                             >
