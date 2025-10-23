a5140c635a2e909523b2432d9d7d56e89fb63f52
KimBlazter <blasster35@gmail.com>
2025-10-23 23:21:20 +0200

    fix(footer): correct copyright notice from 'FAHB' to 'FARE'

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,7 +8,10 @@ export default function Footer() {
             <div className="flex w-full flex-row items-end justify-between">
                 <div className="flex flex-row space-x-1 text-xs opacity-75 md:space-x-4 [&>a]:hover:underline">
                     <Link href="/mentions-legales">Mentions Légales</Link>
-                    <span>© {new Date().getFullYear()} FAHB. Tous droits réservés.</span>
+                    <span>
+                        © {new Date().getFullYear()} FARE. Tous droits
+                        réservés.
+                    </span>
                     <Link
                         className="hidden flex-row items-center space-x-1 md:flex"
                         href="/bug-report"
