3d0dd45b53ac732eaf8e943b50cbfbef26ddf620
finxol <git@finxol.io>
2025-12-07 19:49:39 +0100

    debug: running migrations on container startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
+ls -lA
+ls -lA node_modules
+
 echo "[entrypoint] Running Prisma migrations..."
-npx prisma migrate deploy --schema=./prisma/schema.prisma
+node ./node_modules/prisma/build/index.js migrate deploy --schema=./prisma/schema.prisma
 
 echo "[entrypoint] Starting application..."
 exec node server.js
