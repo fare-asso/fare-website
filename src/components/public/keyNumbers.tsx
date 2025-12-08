197137fcde6a30125ddc8fdf1d31992ebb48e0dc
finxol <git@finxol.io>
2025-12-08 17:22:38 +0100

    fix: animated number on home page

--- src/components/public/keyNumbers.tsx
+++ src/components/public/keyNumbers.tsx
@@ -1,6 +1,6 @@
-import { Association } from "@prisma/client";
-import AnimatedNumber from "../ui/animatedNumber";
+import type { Association } from "@prisma/client";
 import prisma from "@/helpers/db";
+import { AutoAnimatedNumber } from "../ui/animated-number";
 
 export default async function KeyNumbers() {
     let associations: Association[] | undefined;
@@ -15,9 +15,8 @@ export default async function KeyNumbers() {
         <div className="mt-2 flex w-[90%] flex-col items-center justify-center space-y-2 space-x-0 md:mt-0 md:ml-4 md:w-2/3 md:flex-row md:space-y-0 md:space-x-2 [&>div]:h-36 md:[&>div]:h-44">
             <div className="bg-fahbyellow flex h-full w-full flex-col items-center justify-center rounded-xl p-4 md:w-1/3 md:p-2">
                 <span className="text-2xl font-semibold text-white md:text-[2.5rem]">
-                    <AnimatedNumber
-                        end={associations ? associations.length : 20}
-                        duration={1.5}
+                    <AutoAnimatedNumber
+                        value={associations ? associations.length : 20}
                     />
                 </span>
                 <span className="text-center text-xl text-white opacity-95 md:p-1">
@@ -27,7 +26,7 @@ export default async function KeyNumbers() {
 
             <div className="bg-fahbyellow flex h-full w-full flex-col items-center justify-center rounded-xl p-4 md:w-1/3 md:p-2">
                 <span className="text-2xl font-semibold text-white md:text-[2.5rem]">
-                    <AnimatedNumber end={88000} duration={3} />
+                    <AutoAnimatedNumber value={88000} />
                 </span>
                 <span className="text-center text-xl text-white opacity-95 md:p-1">
                     Étudiant.e.s
@@ -36,7 +35,7 @@ export default async function KeyNumbers() {
 
             <div className="bg-fahbyellow flex h-full w-full flex-col items-center justify-center rounded-xl p-4 md:w-1/3 md:p-2">
                 <span className="text-2xl font-semibold text-white md:text-[2.5rem]">
-                    <AnimatedNumber end={57} duration={4} />
+                    <AutoAnimatedNumber value={57} />
                 </span>
                 <span className="text-center text-xl text-white opacity-95 md:p-1">
                     Élu.e.s universitaires & CROUS
