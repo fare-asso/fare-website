97732433a91e016d644fcd5930756ef50ce0325f
finxol <git@finxol.io>
2025-12-09 16:54:26 +0100

    fix: don't check env vars at build time
    Since we're building inside a docker container, it's annoying to pass
    everything into the build container

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,3 @@
-import { createJiti } from "jiti"
-
-const jiti = createJiti(import.meta.url, { debug: true })
-await jiti.import("./src/env")
-
 /** @type {import('next').NextConfig} */
 export default {
     output: "standalone",
