226de0599f911a44e77d074c0ac5e0dffb53039a
finxol <git@finxol.io>
2025-12-10 17:19:05 +0100

    chore: allow images from dev supabase instance

--- next.config.mjs
+++ next.config.mjs
@@ -21,11 +21,20 @@ export default {
                 hostname: "ezatoworfypbxlvjkhud.supabase.co",
                 port: "",
                 pathname: "/storage/v1/object/public/**"
+            },
+            {
+                protocol: "http",
+                hostname:
+                    "fare-website-supabase-52fea5-188-34-191-34.traefik.me",
+                port: "",
+                pathname: "/storage/v1/object/public/**"
             }
         ]
     },
 
     /** We already do linting and typechecking as separate tasks in CI */
     eslint: { ignoreDuringBuilds: true },
-    typescript: { ignoreBuildErrors: true }
+    typescript: { ignoreBuildErrors: true },
+
+    devIndicators: false
 }
