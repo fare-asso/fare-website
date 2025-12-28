d59f41456748454878b09b9144ce7efa9a32d0ad
finxol <git@finxol.io>
2025-12-28 17:28:06 +0100

    chore: update deps

---
+++ taze.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "taze"
+
+export default defineConfig({
+    // ignore packages from bumping
+    exclude: [],
+    // fetch latest package info from registry without cache
+    force: false,
+    // write to package.json
+    write: true,
+    // run `npm install` or `yarn install` right after bumping
+    install: true,
+    // ignore paths for looking for package.json in monorepo
+    ignorePaths: ["**/node_modules/**", "**/test/**"],
+    // ignore package.json that in other workspaces (with their own .git,pnpm-workspace.yaml,etc.)
+    ignoreOtherWorkspaces: false,
+    // disable checking for "overrides" package.json field
+    depFields: {
+        overrides: false
+    }
+})
