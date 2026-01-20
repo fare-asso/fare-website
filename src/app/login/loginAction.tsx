1df89243e22297d7fe04984affd1d37c1d8fd1ea
finxol <git@finxol.io>
2026-01-20 10:33:51 +0100

    feat: add google oauth
    client scope is limited to org members

---
+++ src/app/login/loginAction.tsx
@@ -0,0 +1,102 @@
+"use server"
+
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+import { env } from "@/env"
+import { createClient } from "@/helpers/supabase/server"
+import getCurrentUserRole from "@/helpers/user/role"
+
+export async function loginWithGoogleAction() {
+    const supabase = await createClient()
+
+    console.log("loginWithGoogleAction")
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+            redirectTo: new URL(
+                "/login/callback/google",
+                env.NEXT_PUBLIC_SITE_URL
+            ).toString()
+        }
+    })
+
+    if (error) {
+        console.error(error.message)
+        throw new Error("Failed to login with Google")
+    }
+
+    if (data.url) {
+        redirect(data.url)
+    }
+
+    throw new Error("Unexpected error")
+}
+
+export async function loginWithPasswordAction(
+    _currentState: { emailError?: string; passwordError?: string } | undefined,
+    formData: FormData
+) {
+    const supabase = await createClient()
+
+    const email = formData.get("email")?.toString()
+    const password = formData.get("password")?.toString()
+
+    // Fields validation
+    if (!email) {
+        return {
+            emailError: "Veuillez entrer une adresse email"
+        }
+    }
+
+    if (!password) {
+        return {
+            passwordError: "Veuillez entrer un mot de passe"
+        }
+    }
+
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+
+    // Check if the user is authenticated and handle errors
+    if (error) {
+        console.error(error.code)
+        switch (error.code) {
+            case "invalid_credentials":
+                return {
+                    passwordError: "Mot de passe ou nom d'utilisateur invalide"
+                }
+            default:
+                return {
+                    passwordError: "Une erreur inattendue est survenue"
+                }
+        }
+    }
+
+    // fetch user role
+    const { role, error: roleError } = await getCurrentUserRole()
+
+    if (roleError) {
+        return {
+            passwordError:
+                "Impossible de récupérer les informations de l'utilisateur"
+        }
+    }
+
+    // Redirect user based on their role
+    switch (role) {
+        case "ADMIN":
+            revalidatePath("/dashboard")
+            redirect("/dashboard")
+
+        case "ASSO_OWNER":
+            revalidatePath("/espace-asso")
+            redirect("/espace-asso")
+
+        case "MEMBER":
+            revalidatePath("/dashboard")
+            redirect("/dashboard")
+    }
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { LoaderIcon } from "lucide-react"
 import Image from "next/image"
+import { useSearchParams } from "next/navigation"
+import { useActionState, useTransition } from "react"
+import { FcGoogle } from "react-icons/fc"
 import { MdAdminPanelSettings } from "react-icons/md"
 import FARELogo from "#public/logo_fare.png"
-import LoginForm from "@/components/dashboard/loginForm"
+import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
@@ -11,8 +15,27 @@ import {
     CardHeader,
     CardTitle
 } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { loginWithGoogleAction, loginWithPasswordAction } from "./loginAction"
 
 export default function LoginPage() {
+    const searchParams = useSearchParams()
+    const error = searchParams.get("error")
+
+    const [passwordError, loginWithPassword, passwordPending] = useActionState<
+        { emailError?: string; passwordError?: string } | undefined,
+        FormData
+    >(loginWithPasswordAction, { emailError: "", passwordError: "" })
+
+    const [googlePending, startTransition] = useTransition()
+
+    const handleGoogleLogin = () => {
+        startTransition(() => {
+            loginWithGoogleAction()
+        })
+    }
+
     return (
         <Card className="mx-auto mt-20 min-w-1/3 max-w-sm">
             <CardHeader className="flex flex-col items-start">
@@ -26,11 +49,67 @@ export default function LoginPage() {
                     <span>Connexion Admin</span>
                 </CardTitle>
                 <CardDescription>
-                    Accès réservé uniquement aux membres de la fédération
+                    Accès réservé au bureau de la fédération
+                    {error && (
+                        <div className="mt-4 rounded border border-red-500 bg-red-50 p-2 text-red-600">
+                            Une erreur s'est produite lors de la connexion avec
+                            Google. Merci de réessayer plus tard.
+                        </div>
+                    )}
                 </CardDescription>
             </CardHeader>
             <CardContent>
-                <LoginForm></LoginForm>
+                <Button
+                    type="button"
+                    className="h-12 w-96 max-w-full"
+                    variant="outline"
+                    onClick={handleGoogleLogin}
+                    disabled={googlePending}
+                >
+                    {googlePending ? (
+                        <>
+                            <LoaderIcon className="animation-duration-[1500ms] animate-spin" />
+                            Redirection...
+                        </>
+                    ) : (
+                        <>
+                            <FcGoogle />
+                            Continuer avec Google
+                        </>
+                    )}
+                </Button>
+                <div className="flex w-96 max-w-full items-center justify-center py-6">
+                    <div className="flex-1 border-gray-300 border-t"></div>
+                    <span className="px-4 text-gray-500 text-sm">ou</span>
+                    <div className="flex-1 border-gray-300 border-t"></div>
+                </div>
+                <form action={loginWithPassword} className="space-y-3">
+                    <Label htmlFor="email">Email</Label>
+                    <Input type="email" name="email" id="email" />
+                    {passwordError?.emailError ? (
+                        <div className="font-medium text-destructive text-sm">
+                            {passwordError.emailError}
+                        </div>
+                    ) : null}
+
+                    <Label htmlFor="password">Password</Label>
+                    <Input type="password" name="password" id="password" />
+                    {passwordError?.passwordError ? (
+                        <div className="font-medium text-destructive text-sm">
+                            {passwordError.passwordError}
+                        </div>
+                    ) : null}
+                    <Button disabled={passwordPending} type="submit">
+                        {passwordPending ? (
+                            <>
+                                <LoaderIcon className="animation-duration-1500ms] animate-spin" />
+                                Connexion en cours
+                            </>
+                        ) : (
+                            "Connexion"
+                        )}
+                    </Button>
+                </form>
             </CardContent>
         </Card>
     )
