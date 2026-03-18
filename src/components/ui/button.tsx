c4d1622eef29905581125c00ff20bae76111e0aa
finxol <git@finxol.io>
2026-03-18 16:48:48 +0100

    fix: type export

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -34,6 +34,8 @@ const buttonVariants = cva(
     }
 )
 
+export type ButtonVariants = VariantProps<typeof buttonVariants>
+
 export interface ButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement>,
         VariantProps<typeof buttonVariants> {
