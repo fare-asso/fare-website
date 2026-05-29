f03bce1e88e64953199e438ad4c51ad435e2c6b5
GIKHAEV <abdoullakh.gkh@gmail.com>
2026-05-29 17:01:32 +0200

    feat: ajout des modèles Elu, Instance, Conseil et migration

---
+++ migrations/20260529145907_add_elu_instance_conseil_models/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "Instance" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "contactEmail" TEXT NOT NULL,
+    "description" TEXT,
+    "logoPath" TEXT,
+    "order" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "Instance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Conseil" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "instanceId" INTEGER NOT NULL,
+
+    CONSTRAINT "Conseil_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Elu" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "position" TEXT NOT NULL,
+    "description" TEXT,
+    "order" INTEGER NOT NULL DEFAULT 0,
+    "conseilId" INTEGER NOT NULL,
+
+    CONSTRAINT "Elu_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Conseil" ADD CONSTRAINT "Conseil_instanceId_fkey" FOREIGN KEY ("instanceId") REFERENCES "Instance"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Elu" ADD CONSTRAINT "Elu_conseilId_fkey" FOREIGN KEY ("conseilId") REFERENCES "Conseil"("id") ON DELETE CASCADE ON UPDATE CASCADE;
