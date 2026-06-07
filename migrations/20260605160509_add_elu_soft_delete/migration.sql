fb65aadf4e2fec31d00c71f61c6c563dc80d307e
finxol <git@finxol.io>
2026-06-08 00:09:14 +0200

    chore: regenerate migration
    cleaner to have a single migration for a PR

--- migrations/20260605160509_add_elu_soft_delete/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Elu" ADD COLUMN     "deletedAt" TIMESTAMP(3);
--- migrations/20260607000000_add_elu_feature/migration.sql
+++ migrations/20260607000000_add_elu_feature/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "Instance" (
     "name" TEXT NOT NULL,
     "contactEmail" TEXT NOT NULL,
     "description" TEXT,
-    "logoPath" TEXT,
+    "logoPaths" TEXT[] DEFAULT ARRAY[]::TEXT[],
     "order" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "Instance_pkey" PRIMARY KEY ("id")
@@ -28,13 +28,21 @@ CREATE TABLE "Elu" (
     "position" TEXT NOT NULL,
     "description" TEXT,
     "order" INTEGER NOT NULL DEFAULT 0,
+    "deletedAt" TIMESTAMP(3),
     "conseilId" INTEGER NOT NULL,
 
     CONSTRAINT "Elu_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "Conseil_instanceId_idx" ON "Conseil"("instanceId");
+
+-- CreateIndex
+CREATE INDEX "Elu_conseilId_idx" ON "Elu"("conseilId");
 
 -- AddForeignKey
 ALTER TABLE "Conseil" ADD CONSTRAINT "Conseil_instanceId_fkey" FOREIGN KEY ("instanceId") REFERENCES "Instance"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Elu" ADD CONSTRAINT "Elu_conseilId_fkey" FOREIGN KEY ("conseilId") REFERENCES "Conseil"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
